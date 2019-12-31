24f4611a2e7f3d18d1809dfacc7f524691f5cf06
LynnL4 <lht856@foxmail.com>
2019-12-31 17:33:39 +0800

    support POSIX

--- src/Seeed_FS.cpp
+++ src/Seeed_FS.cpp
@@ -10,281 +10,376 @@
 #include <Arduino.h>
 #include <Seeed_FS.h>
 
+namespace fs
+{
 
-namespace fs {
-
-File::File(FIL f, const char *n) { //is a file.
+File::File(FIL f, const char *n)
+{ //is a file.
   _file = new FIL(f);
-  if (_file) {
-     _dir = NULL;
-     sprintf((char*)_name, "%s", n);
+  if (_file)
+  {
+    _dir = NULL;
+    sprintf((char *)_name, "%s", n);
   }
 }
 
-File::File(DIR d, const char *n) {// is a directory
+File::File(DIR d, const char *n)
+{ // is a directory
   _dir = new DIR(d);
-  if (_dir) {
+  if (_dir)
+  {
     _file = NULL;
-    sprintf((char*)_name, "%s", n);
+    sprintf((char *)_name, "%s", n);
   }
 }
 
-
-File::File(void) {  
+File::File(void)
+{
   _file = NULL;
   _name[0] = 0;
   //Serial.print("Created empty file object");
 }
 
 // returns a pointer to the file name
-char *File::name(void) {
+char *File::name(void)
+{
   return _name;
 }
 
-
 // a directory is a special type of file
-boolean File::isDirectory(void) {
+boolean File::isDirectory(void)
+{
   FRESULT ret = FR_OK;
   FILINFO v_fileinfo;
-   if((ret = f_stat(_name, &v_fileinfo)) == FR_OK){
-      if (v_fileinfo.fattrib & AM_DIR)
-      {
-        return true;
-      }else{
-        return false;
-      }
-   }else{
+  if ((ret = f_stat(_name, &v_fileinfo)) == FR_OK)
+  {
+    if (v_fileinfo.fattrib & AM_DIR)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else
+  {
     return false;
-   }
-   return false;
+  }
+  return false;
 }
 
-
-size_t File::write(uint8_t val) {
+size_t File::write(uint8_t val)
+{
   return write(&val, 1);
 }
 
-size_t File::write(const uint8_t *buf, size_t size) {
+size_t File::write(const uint8_t *buf, size_t size)
+{
   UINT t;
-  if (!_file) {
+  if (!_file)
+  {
     return 0;
   }
-  if(f_write(_file, buf, size, &t) == -1) // if success, return the number of bytes have written.
+  if (f_write(_file, buf, size, &t) == -1) // if success, return the number of bytes have written.
     return 0;
   else
     return t;
 }
 
 //return the peek value
-int File::peek() { 
-  if (! _file) 
+int File::peek()
+{
+  if (!_file)
     return 0;
 
   int c = read();
-  if (c != -1) f_lseek(_file, f_tell(_file) - 1);
+  if (c != -1)
+    f_lseek(_file, f_tell(_file) - 1);
   return c;
 }
 
-// read a value 
-int File::read() {
+// read a value
+int File::read()
+{
   uint8_t val;
-  if (_file){
-     return read(&val, 1) == 1 ? val : -1;
+  if (_file)
+  {
+    return read(&val, 1) == 1 ? val : -1;
   }
   return -1;
 }
 
-long File::read(void *buf, uint32_t nbyte) {
+long File::read(void *buf, uint32_t nbyte)
+{
   UINT t;
-  if (!_file) {
+  if (!_file)
+  {
     return 0;
   }
-  if(f_read(_file, buf, nbyte, &t) == -1)
+  if (f_read(_file, buf, nbyte, &t) == -1)
     return 0;
   else
-  return t;
+    return t;
 }
 
-int File::available() {
-  if (! _file) return 0;
+int File::available()
+{
+  if (!_file)
+    return 0;
 
-   return !f_eof(_file);
- 
+  return !f_eof(_file);
 }
 
-void File::flush() {
+void File::flush()
+{
   if (_file)
     f_sync(_file);
 }
 
-boolean File::seek(uint32_t pos) {
-  if (! _file) return false;
+boolean File::seek(uint32_t pos)
+{
+  if (!_file)
+    return false;
 
   return f_lseek(_file, pos);
 }
 
-uint32_t File::position() {
-  if (! _file) return -1;
+bool File::seek(uint32_t pos, SeekMode mode)
+{
+  if (!_file)
+    return false;
+  switch (mode)
+  {
+  case SeekSet:
+    return f_lseek(_file, pos);
+    break;
+  case SeekCur:
+    f_lseek(_file, f_tell(_file) + pos);
+    break;
+  case SeekEnd:
+    return f_lseek(_file, f_size(_file)-pos);
+    break;
+  default : return false;
+    break;
+  }
+  return false;
+}
+
+uint32_t File::position()
+{
+  if (!_file)
+    return -1;
   return f_tell(_file);
 }
 
-uint32_t File::size() {
+uint32_t File::size()
+{
   return f_size(_file);
 }
 
-void File::close() {
-  if(isDirectory()){
+void File::close()
+{
+  if (isDirectory())
+  {
     f_closedir(_dir);
     delete _dir;
     _dir = NULL;
-  }else{
-     Serial.println(f_close(_file));
-    delete _file; 
+  }
+  else
+  {
+    Serial.println(f_close(_file));
+    delete _file;
     _file = NULL;
   }
 }
 
 // allows you to recurse into a directory
-File File::openNextFile(uint8_t mode) {
-    FRESULT res;
-    UINT i;
-    static FILINFO fno;
-    static char path[257];
-    strcpy(path, _name);
-    for (;;) {
-        res = f_readdir(_dir, &fno);                   /* Read a directory item */
-        if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
-        if(fno.fattrib==255) continue;  /*ignore if the addr was removed*/
+File File::openNextFile(uint8_t mode)
+{
+  FRESULT res;
+  UINT i;
+  static FILINFO fno;
+  static char path[257];
+  strcpy(path, _name);
+  for (;;)
+  {
+    res = f_readdir(_dir, &fno); /* Read a directory item */
+    if (res != FR_OK || fno.fname[0] == 0)
+      break; /* Break on error or end of dir */
+    if (fno.fattrib == 255)
+      continue; /*ignore if the addr was removed*/
 
-         i = strlen(path);
-            sprintf((char*)path+i, "/%s",  fno.fname);
-        
-        if (fno.fattrib & AM_DIR) {                    /* It is a directory */
-            DIR dir;
-            if((res = f_opendir(&dir, path)) == FR_OK){
-               return File(dir, path);
-            }
-             else
-               return File();
-        } else {                                       
-            FIL file;
-            if((res = f_open(&file, path, mode)) == FR_OK){
-              return File(file, path);
-            }
-            else
-              return File();
-        }
+    i = strlen(path);
+    sprintf((char *)path + i, "/%s", fno.fname);
+
+    if (fno.fattrib & AM_DIR)
+    { /* It is a directory */
+      DIR dir;
+      if ((res = f_opendir(&dir, path)) == FR_OK)
+      {
+        return File(dir, path);
+      }
+      else
+        return File();
     }
-    return File();
+    else
+    {
+      FIL file;
+      if ((res = f_open(&file, path, mode)) == FR_OK)
+      {
+        return File(file, path);
+      }
+      else
+        return File();
+    }
+  }
+  return File();
 }
 
-
-void File::rewindDirectory(void){  
+void File::rewindDirectory(void)
+{
   if (isDirectory())
-     f_rewinddir(_dir);
+    f_rewinddir(_dir);
 }
 
-File::operator bool() {
-  if (_file || _dir) 
-    return  true;
+File::operator bool()
+{
+  if (_file || _dir)
+    return true;
   return false;
 }
 
-
- File FS::open(const char *filepath, uint8_t mode)
- {
-   FRESULT ret = FR_OK;
-   FILINFO v_fileinfo;
-
-   if(!strcmp(filepath, "/"))
-   {
-      DIR dir;
-      if((ret = f_opendir(&dir, _T("/"))) == FR_OK)
-          return File(dir, filepath);
-         else
-          return File();
-          
-   }
-   
-   if((ret = f_stat(filepath, &v_fileinfo)) == FR_OK){
-      if (v_fileinfo.fattrib & AM_DIR)
-      {
-         DIR dir;
-         if((ret = f_opendir(&dir, filepath)) == FR_OK)
-          return File(dir, filepath);
-         else
-          return File();
-      }else{
-         FIL file;
-         if((ret = f_open(&file, filepath, mode)) == FR_OK)
-           return File(file, filepath);
-         else
-           return File();
-      }
-   }else{
-      FIL file;
-         if((ret = f_open(&file, filepath, mode)) == FR_OK)
-           return File(file, filepath);
-         else
-           return File();
-   }
-    
- }
-
-boolean FS:: exists(const char *filepath)
+File FS::open(const char *filepath, uint8_t mode)
 {
-   FRESULT ret = FR_OK;
-   FILINFO v_fileinfo;
-   if((ret = f_stat(filepath, &v_fileinfo)) == FR_OK){
-      return true;
-   }else{
-      return false;
-   }
+  FRESULT ret = FR_OK;
+  FILINFO v_fileinfo;
+
+  if (!strcmp(filepath, "/"))
+  {
+    DIR dir;
+    if ((ret = f_opendir(&dir, _T("/"))) == FR_OK)
+      return File(dir, filepath);
+    else
+      return File();
+  }
+
+  if ((ret = f_stat(filepath, &v_fileinfo)) == FR_OK)
+  {
+    if (v_fileinfo.fattrib & AM_DIR)
+    {
+      DIR dir;
+      if ((ret = f_opendir(&dir, filepath)) == FR_OK)
+        return File(dir, filepath);
+      else
+        return File();
+    }
+    else
+    {
+      FIL file;
+      if ((ret = f_open(&file, filepath, mode)) == FR_OK)
+        return File(file, filepath);
+      else
+        return File();
+    }
+  }
+  else
+  {
+    FIL file;
+    if ((ret = f_open(&file, filepath, mode)) == FR_OK)
+      return File(file, filepath);
+    else
+      return File();
+  }
 }
 
+File FS::open(const char *filepath, const char *mode)
+{
+  if (strlen(mode) > 3)
+  {
+    return File();
+  }
+  if (!strcmp(mode, "r"))
+    return open(filepath, FA_READ);
+  if (!strcmp(mode, "r+"))
+    return open(filepath, FA_READ | FA_WRITE);
+  if (!strcmp(mode, "w"))
+    return open(filepath, FA_CREATE_ALWAYS | FA_WRITE);
+  if (!strcmp(mode, "w+"))
+    return open(filepath, FA_CREATE_ALWAYS | FA_WRITE | FA_READ);
+  if (!strcmp(mode, "a"))
+    return open(filepath, FA_OPEN_APPEND | FA_WRITE);
+  if (!strcmp(mode, "a+"))
+    return open(filepath, FA_OPEN_APPEND | FA_WRITE | FA_READ);
+  if (!strcmp(mode, "wx"))
+    return open(filepath, FA_CREATE_NEW | FA_WRITE);
+  if (!strcmp(mode, "w+x"))
+    return open(filepath, FA_CREATE_NEW | FA_WRITE | FA_READ);
 
-boolean FS::mkdir(const char *filepath) 
+  return File();
+}
+
+boolean FS::exists(const char *filepath)
+{
+  FRESULT ret = FR_OK;
+  FILINFO v_fileinfo;
+  if ((ret = f_stat(filepath, &v_fileinfo)) == FR_OK)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+boolean FS::mkdir(const char *filepath)
 {
   FRESULT ret = FR_OK;
   ret = f_mkdir(filepath);
-  if(ret == FR_OK)
-     return true;
+  if (ret == FR_OK)
+    return true;
   else
     return false;
 }
 
-boolean FS::rename(const char* pathFrom, const char* pathTo)
+boolean FS::rename(const char *pathFrom, const char *pathTo)
 {
   FRESULT ret = FR_OK;
   ret = f_rename(pathFrom, pathTo);
-  if(ret == FR_OK)
-     return true;
+  if (ret == FR_OK)
+    return true;
   else
     return false;
 }
 
-boolean FS::rmdir(const char *filepath) 
+boolean FS::rmdir(const char *filepath)
 {
-   char file[_MAX_LFN+2]; 
-   FRESULT status;
-   DIR dj;
-   FILINFO fno;
-   status = f_findfirst(&dj, &fno, filepath, _T("*"));
-    while (status == FR_OK && fno.fname[0]) {
-        sprintf((char*)file, "%s/%s", filepath, fno.fname);
-        if (fno.fattrib & AM_DIR){
-            rmdir(file);
-        }
-        else{
-            remove(file);
-        }
-        status = f_findnext(&dj, &fno);
-    }
-   f_closedir(&dj);
-  if(remove(filepath))
+  char file[_MAX_LFN + 2];
+  FRESULT status;
+  DIR dj;
+  FILINFO fno;
+  status = f_findfirst(&dj, &fno, filepath, _T("*"));
+  while (status == FR_OK && fno.fname[0])
   {
-     return true;
-  }else{
+    sprintf((char *)file, "%s/%s", filepath, fno.fname);
+    if (fno.fattrib & AM_DIR)
+    {
+      rmdir(file);
+    }
+    else
+    {
+      remove(file);
+    }
+    status = f_findnext(&dj, &fno);
+  }
+  f_closedir(&dj);
+  if (remove(filepath))
+  {
+    return true;
+  }
+  else
+  {
     return false;
   }
 }
@@ -293,9 +388,9 @@ boolean FS::remove(const char *filepath)
 {
   FRESULT ret = FR_OK;
   ret = f_unlink(filepath);
-  if(ret == FR_OK)
-     return true;
+  if (ret == FR_OK)
+    return true;
   else
     return false;
 }
-};
+}; // namespace fs
