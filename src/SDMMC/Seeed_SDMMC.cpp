38b1710dd939c46b7675bbeeb07275cec35b7eea
Baozhu Zuo <zuobaozhu@gmail.com>
2021-01-09 14:54:22 +0800

    add sdmmc bsp code , the code should be in separate file

--- src/SDMMC/Seeed_SDMMC.cpp
+++ src/SDMMC/Seeed_SDMMC.cpp
@@ -2,6 +2,585 @@
 #include <Seeed_FS.h>
 #include "Arduino.h"
 
+#define SEEED_FS_DEBUG printf
+extern "C" {
+#include "stm32h7xx_hal.h"
+#include "../fatfs/diskio.h"
+#include "../fatfs/ffconf.h"
+#include "../fatfs/ff.h"
+
+/* Common Error codes */
+#define BSP_ERROR_NONE                    0
+#define BSP_ERROR_NO_INIT                -1
+#define BSP_ERROR_WRONG_PARAM            -2
+#define BSP_ERROR_BUSY                   -3
+#define BSP_ERROR_PERIPH_FAILURE         -4
+#define BSP_ERROR_COMPONENT_FAILURE      -5
+#define BSP_ERROR_UNKNOWN_FAILURE        -6
+#define BSP_ERROR_UNKNOWN_COMPONENT      -7
+#define BSP_ERROR_BUS_FAILURE            -8
+#define BSP_ERROR_CLOCK_FAILURE          -9
+#define BSP_ERROR_MSP_FAILURE            -10
+#define BSP_ERROR_FEATURE_NOT_SUPPORTED  -11
+
+/* BSP OSPI error codes */
+#define BSP_ERROR_OSPI_SUSPENDED          -20
+#define BSP_ERROR_OSPI_ASSIGN_FAILURE     -24
+#define BSP_ERROR_OSPI_SETUP_FAILURE      -25
+#define BSP_ERROR_OSPI_MMP_LOCK_FAILURE   -26
+#define BSP_ERROR_OSPI_MMP_UNLOCK_FAILURE -27
+
+/* BSP TS error code */
+#define BSP_ERROR_TS_TOUCH_NOT_DETECTED   -30
+
+/* BSP BUS error codes */
+#define BSP_ERROR_BUS_TRANSACTION_FAILURE    -100
+#define BSP_ERROR_BUS_ARBITRATION_LOSS       -101
+#define BSP_ERROR_BUS_ACKNOWLEDGE_FAILURE    -102
+#define BSP_ERROR_BUS_PROTOCOL_FAILURE       -103
+
+#define BSP_ERROR_BUS_MODE_FAULT             -104
+#define BSP_ERROR_BUS_FRAME_ERROR            -105
+#define BSP_ERROR_BUS_CRC_ERROR              -106
+#define BSP_ERROR_BUS_DMA_FAILURE            -107
+
+#define SD_INSTANCES_NBR         2UL
+
+
+#define SD_TIMEOUT 3 * 1000
+#define SD_DEFAULT_BLOCK_SIZE 512
+
+#ifndef SD_WRITE_TIMEOUT
+#define SD_WRITE_TIMEOUT         100U
+#endif
+
+#ifndef SD_READ_TIMEOUT
+#define SD_READ_TIMEOUT          100U
+#endif
+
+/**
+  * @brief  SD transfer state definition
+  */
+#define   SD_TRANSFER_OK         0U
+#define   SD_TRANSFER_BUSY       1U
+
+/**
+  * @brief SD-detect signal
+  */
+#define SD_PRESENT               1U
+#define SD_NOT_PRESENT           0U
+
+#define SECTORSIZE 4096
+
+
+static SD_HandleTypeDef hsd1;
+static volatile DSTATUS Stat = STA_NOINIT;
+static volatile  UINT  WriteStatus = 0, ReadStatus = 0;
+
+/**
+ * @brief  Configure SDMMC clock
+ * @param  None
+ * @retval None
+ */
+void SD_ClockConfig(void){
+    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SDMMC|RCC_PERIPHCLK_SAI4A
+                                |RCC_PERIPHCLK_SAI4B|RCC_PERIPHCLK_I2C4;
+    PeriphClkInitStruct.PLL2.PLL2M = 2;
+    PeriphClkInitStruct.PLL2.PLL2N = 26;
+    PeriphClkInitStruct.PLL2.PLL2P = 2;
+    PeriphClkInitStruct.PLL2.PLL2Q = 2;
+    PeriphClkInitStruct.PLL2.PLL2R = 1;
+    PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_3;
+    PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOWIDE;
+    PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
+    PeriphClkInitStruct.SdmmcClockSelection = RCC_SDMMCCLKSOURCE_PLL;
+    PeriphClkInitStruct.I2c4ClockSelection = RCC_I2C4CLKSOURCE_D3PCLK1;
+    PeriphClkInitStruct.Sai4AClockSelection = RCC_SAI4ACLKSOURCE_PLL2;
+    PeriphClkInitStruct.Sai4BClockSelection = RCC_SAI4BCLKSOURCE_PLL2;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    __HAL_RCC_SDMMC1_FORCE_RESET();
+    __HAL_RCC_SDMMC1_RELEASE_RESET();
+}
+
+/**
+* @brief SD MSP Initialization
+* This function configures the hardware resources used in this example
+* @param hsd: SD handle pointer
+* @retval None
+*/
+void HAL_SD_MspInit(SD_HandleTypeDef* hsd)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hsd->Instance==SDMMC1)
+  {
+  /* USER CODE BEGIN SDMMC1_MspInit 0 */
+
+  /* USER CODE END SDMMC1_MspInit 0 */
+    /* Peripheral clock enable */
+    __HAL_RCC_SDMMC1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+    /**SDMMC1 GPIO Configuration
+    PC12     ------> SDMMC1_CK
+    PC10     ------> SDMMC1_D2
+    PC11     ------> SDMMC1_D3
+    PD2     ------> SDMMC1_CMD
+    PC9     ------> SDMMC1_D1
+    PC8     ------> SDMMC1_D0
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_9
+                          |GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_SDMMC1;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF12_SDMMC1;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    /* SDMMC1 interrupt Init */
+    HAL_NVIC_SetPriority(SDMMC1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(SDMMC1_IRQn);
+  /* USER CODE BEGIN SDMMC1_MspInit 1 */
+
+  /* USER CODE END SDMMC1_MspInit 1 */
+  }
+
+}
+
+/**
+* @brief SD MSP De-Initialization
+* This function freeze the hardware resources used in this example
+* @param hsd: SD handle pointer
+* @retval None
+*/
+void HAL_SD_MspDeInit(SD_HandleTypeDef* hsd)
+{
+  if(hsd->Instance==SDMMC1)
+  {
+  /* USER CODE BEGIN SDMMC1_MspDeInit 0 */
+
+  /* USER CODE END SDMMC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SDMMC1_CLK_DISABLE();
+
+    /**SDMMC1 GPIO Configuration
+    PC12     ------> SDMMC1_CK
+    PC10     ------> SDMMC1_D2
+    PC11     ------> SDMMC1_D3
+    PD2     ------> SDMMC1_CMD
+    PC9     ------> SDMMC1_D1
+    PC8     ------> SDMMC1_D0
+    */
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_12|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_9
+                          |GPIO_PIN_8);
+
+    HAL_GPIO_DeInit(GPIOD, GPIO_PIN_2);
+
+    /* SDMMC1 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(SDMMC1_IRQn);
+  /* USER CODE BEGIN SDMMC1_MspDeInit 1 */
+
+  /* USER CODE END SDMMC1_MspDeInit 1 */
+  }
+
+}
+
+
+
+/**
+  * @brief SDMMC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+uint8_t MX_SDMMC1_SD_Init(void)
+{
+
+  /* USER CODE BEGIN SDMMC1_Init 0 */
+
+  /* USER CODE END SDMMC1_Init 0 */
+
+  /* USER CODE BEGIN SDMMC1_Init 1 */
+
+  /* USER CODE END SDMMC1_Init 1 */
+  hsd1.Instance = SDMMC1;
+  hsd1.Init.ClockEdge = SDMMC_CLOCK_EDGE_RISING;
+  hsd1.Init.ClockPowerSave = SDMMC_CLOCK_POWER_SAVE_DISABLE;
+  hsd1.Init.BusWide = SDMMC_BUS_WIDE_4B;
+  hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
+  hsd1.Init.ClockDiv = SDMMC_HSpeed_CLK_DIV;
+  hsd1.Init.TranceiverPresent = SDMMC_TRANSCEIVER_NOT_PRESENT;
+  if (HAL_SD_Init(&hsd1) != HAL_OK)
+  {
+    return  BSP_ERROR_NO_INIT;
+  }
+  /* USER CODE BEGIN SDMMC1_Init 2 */
+
+  /* USER CODE END SDMMC1_Init 2 */
+    return HAL_OK;
+}
+
+
+/**
+  * @brief  Reads block(s) from a specified address in an SD card, in DMA mode.
+  * @param  Instance   SD Instance
+  * @param  pData      Pointer to the buffer that will contain the data to transmit
+  * @param  BlockIdx   Block index from where data is to be read
+  * @param  BlocksNbr  Number of SD blocks to read
+  * @retval BSP status
+  */
+int32_t BSP_SD_ReadBlocks_DMA(uint32_t Instance, uint32_t *pData, uint32_t BlockIdx, uint32_t BlocksNbr)
+{
+  int32_t ret;
+
+  if(Instance >= SD_INSTANCES_NBR)
+  {
+    ret = BSP_ERROR_WRONG_PARAM;
+  }
+  else if(HAL_SD_ReadBlocks_DMA(&hsd1, (uint8_t *)pData, BlockIdx, BlocksNbr) != HAL_OK)
+  {
+    ret = BSP_ERROR_PERIPH_FAILURE;
+  }
+  else
+  {
+    ret = BSP_ERROR_NONE;
+  }
+  /* Return BSP status */
+  return ret;
+}
+
+/**
+  * @brief  Writes block(s) to a specified address in an SD card, in DMA mode.
+  * @param  Instance   SD Instance
+  * @param  pData      Pointer to the buffer that will contain the data to transmit
+  * @param  BlockIdx   Block index from where data is to be written
+  * @param  BlocksNbr  Number of SD blocks to write
+  * @retval BSP status
+  */
+int32_t BSP_SD_WriteBlocks_DMA(uint32_t Instance, uint32_t *pData, uint32_t BlockIdx, uint32_t BlocksNbr)
+{
+  int32_t ret;
+
+  if(Instance >= SD_INSTANCES_NBR)
+  {
+    ret = BSP_ERROR_WRONG_PARAM;
+  }
+  else if(HAL_SD_WriteBlocks_DMA(&hsd1, (uint8_t *)pData, BlockIdx, BlocksNbr) != HAL_OK)
+  {
+    ret = BSP_ERROR_PERIPH_FAILURE;
+  }
+  else
+  {
+    ret = BSP_ERROR_NONE;
+  }
+  /* Return BSP status */
+  return ret;
+}
+
+
+/**
+  * @brief  Erases the specified memory area of the given SD card.
+  * @param  Instance   SD Instance
+  * @param  BlockIdx   Block index from where data is to be
+  * @param  BlocksNbr  Number of SD blocks to erase
+  * @retval SD status
+  */
+int32_t BSP_SD_Erase(uint32_t Instance, uint32_t BlockIdx, uint32_t BlocksNbr)
+{
+  int32_t ret;
+
+  if(Instance >= SD_INSTANCES_NBR)
+  {
+    ret = BSP_ERROR_WRONG_PARAM;
+  }
+  else if(HAL_SD_Erase(&hsd1, BlockIdx, BlockIdx + BlocksNbr) != HAL_OK)
+  {
+    ret = BSP_ERROR_PERIPH_FAILURE;
+  }
+  else
+  {
+    ret = BSP_ERROR_NONE;
+  }
+  /* Return BSP status */
+  return ret;
+}
+
+/**
+  * @brief  Gets the current SD card data status.
+  * @param  Instance  SD Instance
+  * @retval Data transfer state.
+  *          This value can be one of the following values:
+  *            @arg  SD_TRANSFER_OK: No data transfer is acting
+  *            @arg  SD_TRANSFER_BUSY: Data transfer is acting
+  */
+int32_t BSP_SD_GetCardState(uint32_t Instance)
+{
+  return (int32_t)((HAL_SD_GetCardState(&hsd1) == HAL_SD_CARD_TRANSFER ) ? SD_TRANSFER_OK : SD_TRANSFER_BUSY);
+}
+
+/**
+  * @brief  Get SD information about specific SD card.
+  * @param  Instance  SD Instance
+  * @param  CardInfo  Pointer to HAL_SD_CardInfoTypedef structure
+  * @retval BSP status
+  */
+int32_t BSP_SD_GetCardInfo(uint32_t Instance, HAL_SD_CardInfoTypeDef *CardInfo)
+{
+  int32_t ret;
+
+  if(Instance >= SD_INSTANCES_NBR)
+  {
+    ret = BSP_ERROR_WRONG_PARAM;
+  }
+  else if(HAL_SD_GetCardInfo(&hsd1, CardInfo) != HAL_OK)
+  {
+    ret = BSP_ERROR_PERIPH_FAILURE;
+  }
+  else
+  {
+    ret = BSP_ERROR_NONE;
+  }
+  /* Return BSP status */
+  return ret;
+}
+
+
+static DSTATUS SD_CheckStatus(BYTE lun)
+{
+  Stat = STA_NOINIT;
+
+  if(BSP_SD_GetCardState(0) == BSP_ERROR_NONE)
+  {
+    Stat &= ~STA_NOINIT;
+  }
+
+  return Stat;
+}
+
+/**
+  * @brief  Initializes a Drive
+  * @param  lun : not used
+  * @retval DSTATUS: Operation status
+  */
+DSTATUS SD_initialize(BYTE lun)
+{
+  if(MX_SDMMC1_SD_Init()== BSP_ERROR_NONE)
+  {
+    Stat = SD_CheckStatus(lun);
+  }
+  return Stat;
+}
+
+/**
+  * @brief  Gets Disk Status
+  * @param  lun : not used
+  * @retval DSTATUS: Operation status
+  */
+DSTATUS SD_status(BYTE lun)
+{
+  return SD_CheckStatus(lun);
+}
+
+/**
+  * @brief  Reads Sector(s)
+  * @param  lun : not used
+  * @param  *buff: Data buffer to store read data
+  * @param  sector: Sector address (LBA)
+  * @param  count: Number of sectors to read (1..128)
+  * @retval DRESULT: Operation result
+  */
+DRESULT SD_read(BYTE lun, BYTE *buff, DWORD sector, UINT count)
+{
+  DRESULT res = RES_ERROR;
+  ReadStatus = 0;
+  uint32_t timeout;
+#if (ENABLE_SD_DMA_CACHE_MAINTENANCE == 1)
+  uint32_t alignedAddr;
+#endif
+
+  if(BSP_SD_ReadBlocks_DMA(0, (uint32_t*)buff,
+                           (uint32_t) (sector),
+                           count) == BSP_ERROR_NONE)
+  {
+    /* Wait that the reading process is completed or a timeout occurs */
+    timeout = HAL_GetTick();
+    while((ReadStatus == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
+    {
+    }
+    /* incase of a timeout return error */
+    if (ReadStatus == 0)
+    {
+      res = RES_ERROR;
+    }
+    else
+    {
+      ReadStatus = 0;
+      timeout = HAL_GetTick();
+
+      while((HAL_GetTick() - timeout) < SD_TIMEOUT)
+      {
+        if (BSP_SD_GetCardState(0) == SD_TRANSFER_OK)
+        {
+          res = RES_OK;
+#if (ENABLE_SD_DMA_CACHE_MAINTENANCE == 1)
+            /*
+               the SCB_InvalidateDCache_by_Addr() requires a 32-Byte aligned address,
+               adjust the address and the D-Cache size to invalidate accordingly.
+             */
+            alignedAddr = (uint32_t)buff & ~0x1F;
+            SCB_InvalidateDCache_by_Addr((uint32_t*)alignedAddr, count*BLOCKSIZE + ((uint32_t)buff - alignedAddr));
+#endif
+           break;
+        }
+      }
+    }
+  }
+
+  return res;
+}
+
+/**
+  * @brief  Writes Sector(s)
+  * @param  lun : not used
+  * @param  *buff: Data to be written
+  * @param  sector: Sector address (LBA)
+  * @param  count: Number of sectors to write (1..128)
+  * @retval DRESULT: Operation result
+  */
+DRESULT SD_write(BYTE lun, const BYTE *buff, DWORD sector, UINT count)
+{
+  DRESULT res = RES_ERROR;
+  WriteStatus = 0;
+  uint32_t timeout;
+ /*
+  * since the MPU is configured as write-through, see main.c file, there isn't any need
+  * to maintain the cache as its content is always coherent with the memory.
+  * If needed, check the file "Middlewares/Third_Party/FatFs/src/drivers/sd_diskio_dma_template.c"
+  * to see how the cache is maintained during the write operations.
+  */
+
+  if(BSP_SD_WriteBlocks_DMA(0, (uint32_t*)buff,
+                            (uint32_t)(sector),
+                            count) == BSP_ERROR_NONE)
+  {
+    /* Wait that writing process is completed or a timeout occurs */
+
+    timeout = HAL_GetTick();
+    while((WriteStatus == 0) && ((HAL_GetTick() - timeout) < SD_TIMEOUT))
+    {
+    }
+    /* incase of a timeout return error */
+    if (WriteStatus == 0)
+    {
+      res = RES_ERROR;
+    }
+    else
+    {
+      WriteStatus = 0;
+      timeout = HAL_GetTick();
+
+      while((HAL_GetTick() - timeout) < SD_TIMEOUT)
+      {
+        if (BSP_SD_GetCardState(0) == SD_TRANSFER_OK)
+        {
+          res = RES_OK;
+          break;
+        }
+      }
+    }
+  }
+
+  return res;
+}
+
+/**
+  * @brief  I/O control operation
+  * @param  lun : not used
+  * @param  cmd: Control code
+  * @param  *buff: Buffer to send/receive control data
+  * @retval DRESULT: Operation result
+  */
+DRESULT SD_ioctl(BYTE lun, BYTE cmd, void *buff)
+{
+  DRESULT res = RES_ERROR;
+  HAL_SD_CardInfoTypeDef CardInfo;
+
+  if (Stat & STA_NOINIT) return RES_NOTRDY;
+
+  switch (cmd)
+  {
+  /* Make sure that no pending write process */
+  case CTRL_SYNC :
+    res = RES_OK;
+    break;
+
+  /* Get number of sectors on the disk (DWORD) */
+  case GET_SECTOR_COUNT :
+    BSP_SD_GetCardInfo(0, &CardInfo);
+    *(DWORD*)buff = CardInfo.LogBlockNbr;
+    res = RES_OK;
+    break;
+
+  /* Get R/W sector size (WORD) */
+  case GET_SECTOR_SIZE :
+    BSP_SD_GetCardInfo(0, &CardInfo);
+    *(WORD*)buff = CardInfo.LogBlockSize;
+    res = RES_OK;
+    break;
+
+  /* Get erase block size in unit of sector (DWORD) */
+  case GET_BLOCK_SIZE :
+    BSP_SD_GetCardInfo(0, &CardInfo);
+    *(DWORD*)buff = CardInfo.LogBlockSize / SD_DEFAULT_BLOCK_SIZE;
+	res = RES_OK;
+    break;
+
+  default:
+    res = RES_PARERR;
+  }
+
+  return res;
+}
+
+/**
+  * @brief Rx Transfer completed callbacks
+  * @param hsd: SD handle
+  * @retval None
+  */
+void HAL_SD_RxCpltCallback(SD_HandleTypeDef *hsd)
+{
+//   SCB_InvalidateDCache_by_Addr((uint32_t*)aRxBuffer, BUFFER_WORD_SIZE*4);
+//   RxCplt=1;
+    ReadStatus = 1;
+}
+
+/**
+  * @brief Tx Transfer completed callbacks
+  * @param hsd: SD handle
+  * @retval None
+  */
+void HAL_SD_TxCpltCallback(SD_HandleTypeDef *hsd)
+{
+//   TxCplt=1;
+    WriteStatus = 1;
+}
+
+
+} //extern "C"
+
+
+
+
 namespace fs {
 
     boolean SDMMCFS::begin(uint8_t ssPin, int hz) {
@@ -15,6 +594,43 @@ namespace fs {
         // } else {
         //     return true;
         // }
+        _pdrv = 0xff;
+        if (ff_diskio_get_drive(&_pdrv) != 0 || _pdrv == 0xFF) {
+            return false;
+        }      
+        // ardu_sfud_t* flash_t = (ardu_sfud_t*)malloc(sizeof(ardu_sfud_t));
+        // flash_t->type = FLASH_NONE;
+        // flash_t->status = STA_NOINIT;
+        // flash_t->sector_size = SECTORSIZE; 
+        
+        // s_sfuds[_pdrv] = flash_t; 
+        static const ff_diskio_impl_t flash_impl = {
+            .init = &SD_initialize,
+            .status = &SD_status,
+            .read = &SD_read,
+            .write = &SD_write,
+            .ioctl = &SD_ioctl
+        };
+        ff_diskio_register(_pdrv, &flash_impl);
+        FRESULT status;
+        _drv[0] = _T('0' + _pdrv);
+        status = f_mount(&rootFLASH, _drv, 1);
+        SEEED_FS_DEBUG("The available drive number : %d",_pdrv);
+        SEEED_FS_DEBUG("The status of f_mount : %d",status);
+        SEEED_FS_DEBUG("more information about the status , you can view the FRESULT enum");
+        if (status == FR_NO_FILESYSTEM){
+            BYTE work[SECTORSIZE]; /* Work area (larger is better for processing time) */
+            FRESULT ret;
+            ret = f_mkfs(_drv, FM_FAT, 0, work, sizeof(work));
+            SEEED_FS_DEBUG("The status of f_mkfs : %d",ret);
+            SEEED_FS_DEBUG("more information about the status , you can view the FRESULT enum");            
+            status = f_mount(&rootFLASH,_drv, 1);
+        }
+        if (status != FR_OK) {
+            return false;
+        } else {
+            return true;
+        }
         return true;
     }
 
